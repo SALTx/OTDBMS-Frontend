5de83e5a8fae561fb0606471703ffdb9f34202ce
191203G <94876843+191203G@users.noreply.github.com>
2023-07-27 10:16:35 +0800

    not working js and ejs

---
+++ public/js/trip.js
@@ -0,0 +1,20 @@
+// Event listener for the Study Stage checkboxes
+const studyStageCheckboxes = document.querySelectorAll(".study-stage-checkbox");
+studyStageCheckboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", () => {
+    const checkedStages = Array.from(studyStageCheckboxes)
+      .filter((checkbox) => checkbox.checked)
+      .map((checkbox) => parseInt(checkbox.dataset.stage));
+
+    const tripRows = document.querySelectorAll(".checkbox-table + .trip-table tbody tr");
+
+    tripRows.forEach((row) => {
+      const rowStage = parseInt(row.querySelector("td").textContent);
+      if (checkedStages.includes(rowStage)) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    });
+  });
+});
